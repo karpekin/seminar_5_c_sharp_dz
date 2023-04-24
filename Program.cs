cb1eaecb82264185c738723c4334f1d3f1e69e8c
karpekin <karpekin@mail.ru>
2023-04-24 21:10:48 +0300

    task36

--- Program.cs
+++ Program.cs
@@ -2,13 +2,26 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] array = new int[]{345, 897, 568, 234};
+// int[] array = new int[]{345, 897, 568, 234};
+// int sum = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i]%2 == 0)
+//     {
+//         sum = sum + 1;
+//     }
+// }
+// Console.Write(sum);
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] array = new int[4];
 int sum = 0;
 for (int i = 0; i < array.Length; i++)
-{
-    if (array[i]%2 == 0)
     {
-        sum = sum + 1;
+        array[i] = new Random().Next(3,12);
+        sum = sum + array[i];
     }
-}
+Console.WriteLine(String.Join(",",array));
 Console.Write(sum);
